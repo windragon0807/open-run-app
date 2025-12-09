644e80ec4a855136efe568af27f6707898d05e28
windragon0807 <tmdfyd1995@gmail.com>
2025-12-09 18:09:37 +0900

    fix: URL 변경

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 const HomeURL = "https://open-run.vercel.app/";
 const DevURL = "https://e3ce9e0fee5f.ngrok.app/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
 
-export const URL = DevURL;
+export const URL = HomeURL;
