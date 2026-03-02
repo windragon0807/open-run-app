8676547ef176da408586f668b36dad1a1d39abb4
windragon0807 <tmdfyd1995@gmail.com>
2026-03-02 23:13:16 +0900

    chore(config): default app URL to production endpoint

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 const HomeURL = "https://open-run.vercel.app/";
 const DevURL = "https://laughably-unblended-tiera.ngrok-free.dev/"; // 여기에 실제 컴퓨터의 IP 주소를 입력하세요
 
-export const URL = DevURL;
+export const URL = HomeURL;
